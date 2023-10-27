4581be54ad7d7990acef68eaee5908b1e1ad9789
Steven McCombe <smccombe93@gmail.com>
2023-10-26 20:34:21 -0400

    added basic functions and api testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
 
 function App() {
+  const [service, setService] = useState('');
+  const [description, setDescription] = useState('');
+  const [result, setResult] = useState('');
+
+
+async function handleSubmit() {
+  const openai = require('openai');
+  const api = new openai.API({ key: process.env.REACT_APP_OPENAPI_KEY, engine: 'davinci' });
+
+  try {
+    const response = await api.createCompletion({
+      prompt: `Service: ${service}\nDescription: ${description}`,
+      temperature: 0.7,
+      max_tokens: 150,
+    });
+    const result = response.choices[0].text.trim();
+    setResult(result);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+
+function copyToClipboard() {
+  navigator.clipboard.writeText(result);
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={(e) => setService(e.target.value)}>
+        <option value="">Select Service</option>
+        <option value="Google Sheets">Google Sheets</option>
+        <option value="Microsoft Excel">Microsoft Excel</option>
+      </select>
+      <textarea
+        placeholder="Describe your problem or formula here..."
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button onClick={handleSubmit}>Submit</button>
+      <div className="result">
+        {result}
+        {result && (
+          <button onClick={copyToClipboard}>Copy to Clipboard</button>
+        )}
+      </div>
     </div>
   );
 }
